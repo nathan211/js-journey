bf3be17dce985e6ae9514397c6f99e7c1925924a
Nathan <vietphi99dh@gmail.com>
2020-07-07 17:54:51 +0700

    add lesson 35

---
+++ 35-elseif/index.js
@@ -0,0 +1,20 @@
+function getTicketFee(person){
+	var basePrice = 10000;
+
+	if(person.age < 15){
+		return basePrice * 0.8;
+	}
+	else if(person.age > 60){
+		return basePrice * 0.9;
+	}
+	else{
+		return basePrice;
+	}
+}
+
+var person = {
+	age: 80
+}
+
+var fee = getTicketFee(person);
+console.log(fee);
